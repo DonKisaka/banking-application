9ff95612d9574ff0c7c168d94e5cb66285187abe
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-09 10:26:28 +0300

    AuditLog

---
+++ banking-application/src/main/java/com/banking_application/model/AuditLog.java
@@ -0,0 +1,58 @@
+package com.banking_application.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "auditlogs",
+  indexes = {
+          @Index(name = "idx_audit_user_id", columnList = "user_id"),
+          @Index(name = "idx_audit_action", columnList = "action"),
+          @Index(name = "idx_audit_created_at", columnList = "created_at"),
+          @Index(name = "idx_audit_outcome", columnList = "outcome")
+  })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String action;
+
+    @Column(name = "ip_address")
+    private String ipAddress;
+
+    @Column(name = "user_agent")
+    private String userAgent;
+
+    @Column(name = "affected_resource")
+    private String affectedResource;
+
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private AuditStatus outcome = AuditStatus.SUCCESS;
+
+    @Column(columnDefinition = "TEXT")
+    private String details;
+
+    @Column(name = "error_message")
+    private String errorMessage;
+
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+}

01e55fca83a10882ce9d9e5ebccccd54a78f7ff9
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-11 10:55:51 +0300

    Added new methods

--- banking-application/src/main/java/com/banking_application/repository/FraudAlertRepository.java
+++ banking-application/src/main/java/com/banking_application/repository/FraudAlertRepository.java
@@ -2,15 +2,25 @@ package com.banking_application.repository;
 
 import com.banking_application.model.FraudAlert;
 import com.banking_application.model.FraudStatus;
+import com.banking_application.model.Transaction;
+import com.banking_application.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FraudAlertRepository extends JpaRepository <FraudAlert, Long> {
     List<FraudAlert> findByStatus(FraudStatus status);
 
-    @Query("SELECT f FROM FraudAlert f WHERE f.riskScore >= :minScore")
-    List<FraudAlert> findHighRiskAlerts(@Param("minScore") Integer minScore);
+    List<FraudAlert> findByRiskScoreGreaterThanEqual(Integer riskScore);
+
+    List<FraudAlert> findByStatusOrderByRiskScoreDesc(FraudStatus status);
+
+    Optional<FraudAlert> findByTransaction(Transaction transaction);
+
+    List<FraudAlert> findByUserOrderByCreatedAtDesc(User user);
+
+    boolean existsByUserAndStatus(User user, FraudStatus status);
 }

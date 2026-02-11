0f464572fcd63a013b07478ee51727711a1a90bb
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-11 10:42:21 +0300

    Added new methods

--- banking-application/src/main/java/com/banking_application/repository/AuditLogRepository.java
+++ banking-application/src/main/java/com/banking_application/repository/AuditLogRepository.java
@@ -1,12 +1,27 @@
 package com.banking_application.repository;
 
 import com.banking_application.model.AuditLog;
+import com.banking_application.model.AuditStatus;
 import com.banking_application.model.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
     Page<AuditLog> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
+
     Page<AuditLog> findByAction(String action, Pageable pageable);
+
+    List<AuditLog> findTop10ByUserOrderByCreatedAtDesc(User user);
+
+    Page<AuditLog> findByActionAndOutcome(String action, AuditStatus outcome, Pageable pageable);
+
+    @Query("SELECT COUNT(a) FROM AuditLog a WHERE a.action = 'LOGIN_FAILED' AND a.ipAddress = :ipAddress AND a.createdAt > :since")
+    long countFailedLoginsByIpSince(@Param("ipAddress") String ipAddress, @Param("since") LocalDateTime since);
+
 }

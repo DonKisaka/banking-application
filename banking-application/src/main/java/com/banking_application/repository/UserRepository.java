e68572ccf33bca8172e90fa368ba7c6af44b812e
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-13 11:17:16 +0300

    changed to UUID

--- banking-application/src/main/java/com/banking_application/repository/UserRepository.java
+++ banking-application/src/main/java/com/banking_application/repository/UserRepository.java
@@ -4,13 +4,14 @@ import com.banking_application.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
-    Optional<User> findByUserUuid(String userUuid);
+    Optional<User> findByUserUuid(UUID userUuid);
 
     boolean existsByUsernameOrEmail(String username, String email);
 
-    boolean existsByUserUuid(String userUuid);
+    boolean existsByUserUuid(UUID userUuid);
 }

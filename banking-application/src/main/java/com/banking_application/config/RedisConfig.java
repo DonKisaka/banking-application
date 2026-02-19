61953cf9697a0d51c70b055e0b4b5f8e7f46c20e
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-19 15:19:24 +0300

    Redis Configuration

---
+++ banking-application/src/main/java/com/banking_application/config/RedisConfig.java
@@ -0,0 +1,44 @@
+package com.banking_application.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.Map;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    @Bean
+    @ConditionalOnBean(RedisConnectionFactory.class)
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(GenericJacksonJsonRedisSerializer.builder().build()));
+
+        Map<String, RedisCacheConfiguration> perCacheConfig = Map.of(
+                "accountDetails", defaultConfig.entryTtl(Duration.ofMinutes(30)),
+                "transactionHistory", defaultConfig.entryTtl(Duration.ofMinutes(5)),
+                "transactionByRef", defaultConfig.entryTtl(Duration.ofHours(1))
+        );
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(perCacheConfig)
+                .transactionAware()
+                .build();
+    }
+}

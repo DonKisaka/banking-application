5c407d063e2ae9d1d681271577b00bab7692a36b
DonaldKisaka <davidkisaka4@gmail.com>
2026-02-20 09:31:34 +0300

    Revert "Added hibernate envers methods"
    This reverts commit 8032aadf7d751587c22e02b43800b26d3b8e65dd.

--- banking-application/src/main/java/com/banking_application/dto/RevisionInfoDto.java
+++
@@ -1,8 +0,0 @@
-package com.banking_application.dto;
-
-import java.time.Instant;
-
-public record RevisionInfoDto(
-        Number revisionNumber,
-        Instant revisionTimestamp
-) {}
--- banking-application/src/main/java/com/banking_application/mapper/AccountMapper.java
+++ banking-application/src/main/java/com/banking_application/mapper/AccountMapper.java
@@ -8,7 +8,6 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
-    @Mapping(target = "accountStatus", source = "status")
     AccountResponseDto toDto(Account account);
 
     @Mapping(target = "id", ignore = true)
